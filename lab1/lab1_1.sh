0f1717816f5316a6fc15a5a1929a282a8bed38e4
lukaszfabia <ufabia03@gmail.com>
2024-03-13 10:47:41 +0100

    dwa zad

--- lab1/lab1_1.sh
+++ lab1/lab1_1.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "Jestes tu: $PWD"
+echo "youre here $PWD"
 echo "path to home dir: $HOME"
 echo "username: $USER"
 echo "hostname: $HOSTNAME"
 echo "your os: $OSTYPE"
 
+export MYVAR="2003"
+echo "My var: $MYVAR"
+#usuwam zmienna zeby nie zasmiecac sobie sysemu
+unset MYVAR
